e1e0e5d6437d6afdf088f2f77b7ad70a340c0a26
thibault <thibaultdepeige@gmail.com>
2025-12-01 21:49:50 +0100

    Config modifications

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Projects from "./components/Project";
+import Contact from "./components/Contact";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <About />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Contact />
     </main>
   );
 }
